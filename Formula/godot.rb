b827010ff25cc0ff0e0909efc8e1ad46fcde7a66
Willie169 <cg10250207@gmail.com>
2026-05-10 00:59:50 +0800

    godot

--- Formula/godot.rb
+++ Formula/godot.rb
@@ -1,17 +1,17 @@
 class Godot < Formula
   desc "2D and 3D game engine"
   homepage "https://godotengine.org"
-  license "MIT"
   version "4.6.2"
+  license "MIT"
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/godotengine/godot/releases/download/#{version}-stable/Godot_v#{version}-stable_linux.arm64.zip"
-      sha256 "ARM64_SHA256"
+      sha256 "c9154154de14acb1f38a6c8618f01f4111ecbd1cdbcecd0a5151be42de2bd1c9"
       @_bin_name = "Godot_v#{version}-stable_linux.arm64"
     elsif Hardware::CPU.intel?
       url "https://github.com/godotengine/godot/releases/download/#{version}-stable/Godot_v#{version}-stable_linux.x86_64.zip"
-      sha256 "X86_64_SHA256"
+      sha256 "30e6b6d141f0cd5bebd629ad1d0ef1324e60091bb20662d026b402ba58c59937"
       @_bin_name = "Godot_v#{version}-stable_linux.x86_64"
     else
       odie "Unsupported CPU architecture"
